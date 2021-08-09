7cf12b69ad5e39a6e8ad04d9f291e74404f83dc1
safiur799 <safiurrahaman799@gmail.com>
2021-08-09 14:37:14 +0530

    second 20min

--- coding/script.js
+++ coding/script.js
@@ -1,8 +1,20 @@
+let data_div = document.getElementById("container");
 async function showNews() {
   let res = await fetch(
-    "https://newsapi.org/v2/everything?q=tesla&from=2021-07-09&sortBy=publishedAt&apiKey=e308112c7b5348b7a70a4810ed9cd537"
+    "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=e308112c7b5348b7a70a4810ed9cd537"
   );
   let data = await res.json();
   console.log(data);
+  let artical = data.articles;
+  console.log(artical);
+  showArtcal(artical);
 }
 showNews();
+
+function showArtcal(data) {
+  data.forEach((element) => {
+    let title = document.createElement("p");
+    let img = document.createElement("img");
+    let para = document.createElement("para");
+  });
+}
